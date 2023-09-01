905d5f196d14a91cf6b857b85f5b3d0dcf9fda39
bhupintiwari <bhupintiwari@gmail.com>
2023-09-01 11:20:40 -0500

    Day 1 Test

--- VastikaProject/Models/CustomersModel.cs
+++ VastikaProject/Models/CustomersModel.cs
@@ -4,8 +4,15 @@ namespace VastikaProject.Models;
 
 public class CustomersModel
 {
-   [Key]
+ 
     public int Id{ get; set; }
+    
+    
+    [Required(ErrorMessage = "First Name is required.")]
+    [StringLength(100, ErrorMessage = "First Name cannot be longer than 100 characters.")]
     public string FirstName { get; set; }
+    
+    [Required(ErrorMessage = "Last Name is required.")]
+    [StringLength(100, ErrorMessage = "Last Name cannot be longer than 100 characters.")]
     public string LastName { get; set; }
 }
